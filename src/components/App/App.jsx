357422a863f307f1d064ab6410a8aa2f5c5d2c02
Shobha Chejara <shobha.chejara@gmail.com>
2024-10-18 09:47:06 -0500

    Favorite File work is working in process

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Search from "../Search/Search";
+import Favorite from "../Favorite/Favorite";
 
 
 function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Router>
+        <Route>
       <Search/>
+      </Route>
+      <Route>
+      <Favorite/>
+      </Route>
+      
+      </Router>
     </div>
   );
 }
---
+++ src/components/Favorite/Favorite.jsx
@@ -0,0 +1,16 @@
+
+import React from react;
+import { useDispatch, useSelector } from 'react-redux';
+
+const Favorite = () => {
+    return(
+
+
+        
+    )
+
+
+
+
+}
+export default Favorite;
